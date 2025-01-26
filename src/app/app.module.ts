3ed0d57a175d79f8e903dc97162b25a504da9ca3
Limbureuben <limbureuben4@gmail.com>
2025-01-26 20:22:54 +0300

    The changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './common/homepage/homepage.component';
+import { AboutComponent } from './common/about/about.component';
+import { ContactComponent } from './common/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
