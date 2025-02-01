20313b94b99a78af9c802dcf1f4b42dd8289d7ee
Limbureuben <limbureuben4@gmail.com>
2025-02-01 23:30:16 +0300

    change

--- src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -6,22 +6,7 @@ import { Chart } from 'chart.js/auto';
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
-export class AdminDashboardComponent implements AfterViewInit{
-  ngAfterViewInit() {
-    this.createPieChart();
-  }
+export class AdminDashboardComponent {
 
-  createPieChart() {
-    new Chart("pieChart", {
-      type: 'pie',
-      data: {
-        labels: ['Reports', 'Users', 'Resolved Issues'],
-        datasets: [{
-          data: [120, 50, 80], // Example values
-          backgroundColor: ['#FF6384', '#36A2EB', '#4BC0C0']
-        }]
-      }
-    });
-  }
 
 }
