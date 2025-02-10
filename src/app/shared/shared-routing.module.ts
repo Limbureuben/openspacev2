e35e3ddfb380c4b53eac4168307bb39ae237ed49
Limbureuben <limbureuben4@gmail.com>
2025-02-10 19:26:47 +0300

    change

--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -20,6 +20,8 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   { path: 'admin-header', component: AdminHeaderComponent},
+  { path: 'admin-sidebar', component: AdminSidebarComponent},
+  { path: 'email-form', component: EmailFormComponent}
 ];
 
 @NgModule({
